939e63c941949a14b02e3d895896ffed36b564a4
tupt9x <mr.tus97@gmail.com>
2024-01-17 21:22:21 +0700

    Xây dựng project chuẩn MVVM

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyKhoKteam.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+
+        // Mọi thứ xử lý sẽ nằm trong class này
+
+        public MainViewModel() {
+            MessageBox.Show("Đã vào trong MainViewModel -> Data context của mainwindow.xaml");
+        }
+    }
+}
