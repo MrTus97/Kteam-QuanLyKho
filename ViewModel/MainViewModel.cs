344e2529b9ea62c52185fb2fac01a57c9861fd8c
tupt9x <mr.tus97@gmail.com>
2024-02-01 22:53:26 +0700

    Thiết kế màn hình thêm, xóa, sửa

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -16,6 +16,20 @@ namespace QuanLyKhoKteam.ViewModel
         public bool isLoaded = false;
 
         public ICommand LoadedWindowCommand { get; set; }
+
+        public ICommand UnitCommand { get; set; } 
+
+        public ICommand SuplierCommand { get; set; }
+
+        public ICommand CustomerCommand { get; set; }
+
+        public ICommand ObjectCommmand { get; set; }
+
+        public ICommand UserCommand { get; set; }
+
+        public ICommand InputCommand { get; set; }
+
+        public ICommand OutputCommand { get; set; }
         public MainViewModel() {
             LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
             {
@@ -23,7 +37,45 @@ namespace QuanLyKhoKteam.ViewModel
                 LoginWindow loginWindow = new LoginWindow();
                 loginWindow.ShowDialog();
             });
-            
+
+            UnitCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                UnitWindow wd = new UnitWindow();
+                wd.ShowDialog();
+            });
+
+            SuplierCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                SuplierWindow wd = new SuplierWindow();
+                wd.ShowDialog();
+            });
+
+            CustomerCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                CustomerWindow wd = new CustomerWindow();
+                wd.ShowDialog();
+            });
+            ObjectCommmand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                ObjectWindow wd = new ObjectWindow();
+                wd.ShowDialog();
+            });
+            UserCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                UserWindow wd = new UserWindow();
+                wd.ShowDialog();
+            });
+            InputCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                InputWindow wd = new InputWindow();
+                wd.ShowDialog();
+            });
+
+            OutputCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                OutputWindow wd = new OutputWindow();
+                wd.ShowDialog();
+            });
         }
     }
 }
